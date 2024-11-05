e1b7246c5d70d19c2098cdb69f436ec5c8de9624
winning godspower <winninggodspower@gmail.com>
2024-11-05 11:32:39 +0100

    feat: add annoucment backend func

---
+++ announcement/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Announcement
+
+class AnnouncementForm(forms.ModelForm):
+    class Meta:
+        model = Announcement
+        fields = ['title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
---
+++ announcement/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+
+from announcement.forms import AnnouncementForm
+from announcement.models import Announcement
+
+# Create your views here.
+def announcement_view(request):
+    annoucements = Announcement.objects.al()
+    return render(request, 'announcement.html', {'announcements': annoucements})
+
+
+def addAnnoucement(request):
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = AnnouncementForm()
+    return render(request, 'addAnnouncement.html', {'form': form})
